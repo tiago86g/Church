6619626877b90ead27bb83dd0890c308c4a2e342
tiago <tiago86g@gmail.com>
2020-11-12 13:33:05 +0100

    Setting up React router

--- church/src/App.js
+++ church/src/App.js
@@ -1,7 +1,21 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App">HI CHURCH</div>;
+  return (
+    <div className="App">
+      <Router>
+        {/* navbar component */}
+        <div className="main">
+          <Switch>
+            <Route exact path="/" />
+            {/* pages components */}
+          </Switch>
+        </div>
+        {/* footer component */}
+      </Router>
+    </div>
+  );
 }
 
 export default App;
