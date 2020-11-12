fef52e17ea43c9556a6084c0ec51ff2e8ea28c0b
tiago <tiago86g@gmail.com>
2020-11-12 14:12:33 +0100

    Kanon e Shadi route

--- church/src/App.js
+++ church/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MoezeHa from './components/pages/MoezeHa/MoezeHa';
 import KhaneSorod from './components/pages/KhaneSorod/KhaneSorod';
+import KanonEShadi from './components/pages/KanonEShadi/KanonEShadi';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
             <Route exact path="/" />
             <Route exact path="/moeze-ha" component={MoezeHa} />
             <Route exact path="/khane-sorod" component={KhaneSorod} />
+            <Route exact path="/kanon-e-shadi" component={KanonEShadi} />
 
             {/* pages components */}
           </Switch>
