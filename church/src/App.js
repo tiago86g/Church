67a46040979dba77b84e21f8e248973a065bb21b
tiago <tiago86g@gmail.com>
2020-11-12 14:15:22 +0100

    Akhbar route

--- church/src/App.js
+++ church/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MoezeHa from './components/pages/MoezeHa/MoezeHa';
 import KhaneSorod from './components/pages/KhaneSorod/KhaneSorod';
 import KanonEShadi from './components/pages/KanonEShadi/KanonEShadi';
+import Akhbar from './components/pages/Akhbar/Akhbar';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
             <Route exact path="/moeze-ha" component={MoezeHa} />
             <Route exact path="/khane-sorod" component={KhaneSorod} />
             <Route exact path="/kanon-e-shadi" component={KanonEShadi} />
-
+            <Route exact path="/akhbar" component={Akhbar} />
             {/* pages components */}
           </Switch>
         </div>
